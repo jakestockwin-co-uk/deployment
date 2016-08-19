d789d90c3e7a1a502947589bd3720af8d78b41dc
Ben Spiers <ben.spiers22@gmail.com>
2016-08-19 23:44:40 +0100

    Update Travis deployment script, we have a domain name now \o/

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,5 +2,5 @@
 
 echo "Deploying $TRAVIS_COMMIT"
 
-curl -NX POST http://139.59.176.9:3000/deploy --data "project=$TRAVIS_REPO_SLUG&commit=$TRAVIS_COMMIT&email=$EMAIL&password=$PASSWORD"
+curl -NX POST https://deployment.jakestockwin.co.uk/deploy --data "project=$TRAVIS_REPO_SLUG&commit=$TRAVIS_COMMIT&email=$EMAIL&password=$PASSWORD"
 
