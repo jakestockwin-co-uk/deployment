7f3a873f82c4b7a64dd608342a8a7dd595d76de6
Ben Spiers <ben.spiers22@gmail.com>
2016-08-21 23:19:49 +0100

    Use async/await semantics to make deploy async code nicer

--- routes/views/deploy.js
+++ routes/views/deploy.js
@@ -8,65 +8,64 @@ var async = asyncawait.async;
 var await = asyncawait.await;
 
 exports = module.exports = function (req, res) {
-	var onSuccess = function (user) {
-		var site;
-
+	var onSuccess = async(function (user) {
 		if (!user.canDeploy) {
 			addInfo('User not authorised to deploy new app versions', res);
 			res.end();
 			return;
 		}
 
-		Site.model.findOne().where('githubRepository', req.body.project).exec().then(function (found) {
-			site = found;
+		try {
+
+			let site = await(Site.model.findOne().where('githubRepository', req.body.project).exec());
+
 			// Get all the servers we're not on yet
-			return Deployment.model.find().where('site', site).where('initialised', false).populate('site server').exec();
-		}).then(function (uninitialised) {
-			if (!uninitialised.length) {
-				return;
+			let uninitialised = await(Deployment.model.find().where('site', site).where('initialised', false).populate('site server').exec());
+
+			if (uninitialised.length) {
+				// Initialise on any new servers
+				addInfo('Initalising on ' + uninitialised.length.toString() + ' application servers.', res);
+
+				await(uninitialised.reduce(function (chain, deployment) {
+					return chain.then(function () {
+						// Do the initialisation
+						return initPromise(deployment, res);
+					}).then(function () {
+						// Copy the env over
+						return envPromise(deployment, res);
+					}).then(function () {
+						deployment.initialised = true;
+						return deployment.save();
+					}).catch(function (err) {
+						// If a command failed somewhere, set to uninitialised.
+						console.log('Error initialising server');
+						console.log(err);
+						deployment.initialised = false;
+						return deployment.save();
+					});
+				}, Promise.resolve()));
 			}
-			// Initialise on any new servers
-			addInfo('Initalising on ' + uninitialised.length.toString() + ' application servers.', res);
-			return uninitialised.reduce(function (chain, deployment) {
-				return chain.then(function () {
-					// Do the initialisation
-					return initPromise(deployment, res)
-				}).then(function () {
-					// Copy the env over
-					return envPromise(deployment, res)
-				}).then(function () {
-					deployment.initialised = true;
-					return deployment.save();
-				}).catch(function (err) {
-					// If a command failed somewhere, set to uninitialised.
-					console.log('Error initialising server');
-					console.log(err);
-					deployment.initialised = false;
-					return deployment.save();
-				});
-			}, Promise.resolve());
-		}).then(function () {
+
 			// Get all the servers we're initialised on now
-			return Deployment.model.find().where('site', site).where('initialised', true).populate('site server').exec();
-		}).then(function (initialised) {
-			if (!initialised.length) {
-				return;
+			let initialised = await(Deployment.model.find().where('site', site).where('initialised', true).populate('site server').exec());
+
+			if (initialised.length) {
+				// Update on all the servers we're on
+				addInfo('Deploying update to ' + initialised.length.toString() + ' application servers.', res);
+				await(initialised.reduce(function (chain, deployment) {
+					return chain.then(function () {
+						// Update the deployment
+						return updatePromise(deployment, req.body.commit, res);
+					});
+				}, Promise.resolve()));
 			}
-			// Update on all the servers we're on
-			addInfo('Deploying update to ' + initialised.length.toString() + ' application servers.', res);
-			return initialised.reduce(function(chain, deployment) {
-				return chain.then(function () {
-					// Update the deployment
-					return updatePromise(deployment, req.body.commit, res)
-				});
-			}, Promise.resolve());
-		}).then(null, function (err) { // .catch(function (err { // but mpromise doesn't support catch.
-			console.log("ERROR: ");
+		} catch (err) {
+			console.log('ERROR: ');
 			console.log(err);
-		}).then(function () {
+		} finally {
 			res.end('\n');
-		});
-	};
+		}
+	});
 
 	var onFail = function (err) {
 		var message = (err && err.message) ? err.message : 'Sorry, that email and/or password are not valid.';
