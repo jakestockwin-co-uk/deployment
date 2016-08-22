3bcd766dd7711e5d2d6832ca35c6ffe5d2705f66
Ben Spiers <ben.spiers22@gmail.com>
2016-08-22 21:05:37 +0100

    Make the Travis deploy script fail if deploy failed

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env sh
 
+STATUS=0
+
 echo "Deploying $TRAVIS_COMMIT"
 
-curl -NX POST https://deployment.jakestockwin.co.uk/deploy --data "project=$TRAVIS_REPO_SLUG&commit=$TRAVIS_COMMIT&email=$EMAIL&password=$PASSWORD"
+curl -NX POST https://deployment.jakestockwin.co.uk/deploy --data "project=$TRAVIS_REPO_SLUG&commit=$TRAVIS_COMMIT&email=$EMAIL&password=$PASSWORD" | while IFS= read -r LINE
+do
+	if [[ ${LINE} == "[SUCCESS]" ]]
+	then
+		echo "Deployment successful"
+		STATUS=0
+	elif [[ ${LINE} == "[FAILURE]" ]]
+	then
+		echo "Deployment failed"
+		STATUS=1
+	else
+		echo $LINE
+	fi
+done
 
+exit $STATUS
